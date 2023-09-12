697e6241a03ad392cc44d1b7ef8b658db608b08e
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-12 11:44:14 -0300

    site front para aulas

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import {  } from 'react'
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+     <div className='container'>
+        <section className="estrutura">
+          <article className="box">
+            <h1>Nossa estrutura</h1>
+            <img src="https://picsum.photos/400/300" alt="" />
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae explicabo id ad quisquam fuga deleniti iure, in tempore atque laudantium nesciunt eius. Et magni nesciunt praesentium harum explicabo quo ipsam!</p>
+            <a href="">
+            Veja mais
+          </a> 
+          </article>
+        </section>
+     </div>
+    </>
+  )
+}
+
+export default App
